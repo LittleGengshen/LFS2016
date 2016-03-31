c04250f6569c99f440399490170947d0fdc71160
Yufeng Huang <mildwindyu@hotmail.com>
2016-03-31 23:15:04 +0800

    implemented function Attack

--- ai/funcofallxll.cpp
+++ ai/funcofallxll.cpp
@@ -26,5 +26,27 @@ Position JudgeDirection(const Position & pos)
 }
 void Attack(Action & ret)
 {
+	int dashLevel = info->StatusNow->objects[0].skill_level[DASH];
+	int shortAttackLevel = info->StatusNow->objects[0].skill_level[SHORT_ATTACK];
+	int myID = info->StatusNow->objects[0].id;
+	int objnum = info->MapNow->objects_number;
+	int my01ID = info->StatusNow->team_id;
+	double myradius = info->StatusNow->objects[0].radius;
+	Position mypos = info->StatusNow->objects[0].pos;
+	for (int i = 0; i < objnum; i++)
+	{
+		if (info->MapNow->objects[i].team_id == my01ID^1)
+		{
+			int otherRealID = info->MapNow->objects[i].id;
+			Position otherpos = info->MapNow->objects[i].pos;
+			double otherradius = info->MapNow->objects[i].radius;
+			if (Distance(mypos, otherpos) - myradius - otherradius < kShortAttackRange[shortAttackLevel])
+			{
+				ret.skill_usage[0][SHORT_ATTACK].TargetID = otherRealID;
+				ret.skill_usage[0][SHORT_ATTACK].UserID = myID;
+				ret.skill_usage[0][SHORT_ATTACK].toUseSkill = true;
+			}
+		}
+	}
 
 }
